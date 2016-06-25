2d21d848c3f22631955ed6f9d702a96da13b0bd5
RyanJaunzemis <khuang20@punahou.edu>
2016-06-24 14:46:17 -1000

    worked on project

--- loops.js
+++ loops.js
@@ -1,7 +1,30 @@
 // Do your work here
+/**
 var Presidents = ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe']
 
 for(var i = 0; i < Presidents.length; i++){
 	console.log("The value of i is: " + [i]);
 	console.log("The value at index is: " + Presidents[i]);
 }
+
+
+
+
+
+*/
+/**
+function(string){
+	var stringOfNumbers = [''];
+for(var i = 10; i<= 20; i++){
+	console.log(i);
+} 
+}
+
+
+
+
+
+*/
+
+
+
