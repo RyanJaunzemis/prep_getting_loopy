ef4487fce736ae4a358b1137fcfce4ac3d42b0ee
RyanJaunzemis <khuang20@punahou.edu>
2016-06-27 13:49:47 -1000

    worked on no.2

--- loops.js
+++ loops.js
@@ -12,19 +12,12 @@ for(var i = 0; i < Presidents.length; i++){
 
 
 */
-/**
-function(string){
+
+
 	var stringOfNumbers = [''];
 for(var i = 10; i<= 20; i++){
 	console.log(i);
-} 
 }
-
-
-
-
-
-*/
 
 
 
